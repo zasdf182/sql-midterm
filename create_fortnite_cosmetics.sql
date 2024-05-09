d39a98b9983364176fa22cbd61f362ed1f332137
zasdf182 <35350192+zasdf182@users.noreply.github.com>
2024-05-09 01:43:15 -0700

    Add tables - emotes, pickaxes, loadouts (#2)
    * feat: create tables - emotes, pickaxes, loadouts
    
    * feat: add table data - emotes
    
    * style: update my tables to match main
    
    * feat: add table data - pickaxes

--- create_fortnite_cosmetics.sql
+++ create_fortnite_cosmetics.sql
@@ -37,6 +37,58 @@ CREATE TABLE gliders(
 		REFERENCES bundles (bundle_name)
 );
 
+CREATE TABLE emotes(
+	emote_name		VARCHAR(255)	PRIMARY KEY,
+    emote_id		VARCHAR(255)	NOT NULL		UNIQUE,
+    rarity			VARCHAR(15)		NOT NULL,
+    cost			INT,
+    bundle_name		VARCHAR(255)	DEFAULT NULL,
+    skin_name		VARCHAR(255)	DEFAULT NULL,
+    sync_status     BOOLEAN			NOT NULL		DEFAULT 0,
+    CONSTRAINT emotes_fk_bundles
+        FOREIGN KEY (bundle_name)
+        REFERENCES bundles (bundle_name),
+    CONSTRAINT emotes_fk_skins
+        FOREIGN KEY (skin_name)
+        REFERENCES skins (skin_name)
+);
+
+CREATE TABLE pickaxes(
+	pickaxe_name		VARCHAR(255)	PRIMARY KEY,
+    pickaxe_id			VARCHAR(255)	NOT NULL	    UNIQUE,
+    rarity				VARCHAR(15)		NOT NULL,
+    cost				INT,
+    pickaxe_set_name	VARCHAR(255)	DEFAULT NULL,
+    bundle_name			VARCHAR(255)	DEFAULT NULL,
+    CONSTRAINT pickaxes_fk_bundles
+        FOREIGN KEY (bundle_name)
+        REFERENCES bundles (bundle_name)
+);
+
+CREATE TABLE loadouts(
+	player_name		VARCHAR(255)	PRIMARY KEY,
+    skin_name		VARCHAR(255)	PRIMARY KEY,
+	emote_name		VARCHAR(255)	PRIMARY KEY,
+	pickaxe_name	VARCHAR(255)	PRIMARY KEY,
+	glider_name		VARCHAR(255)	PRIMARY KEY,
+	music_name		VARCHAR(255)	PRIMARY KEY,
+    CONSTRAINT loadouts_fk_skins
+        FOREIGN KEY (skin_name)
+        REFERENCES skins (skin_name),
+    CONSTRAINT loadouts_fk_emotes
+        FOREIGN KEY (emote_name)
+        REFERENCES emotes (emote_name),
+    CONSTRAINT loadouts_fk_pickaxes
+        FOREIGN KEY (pickaxe_name)
+        REFERENCES pickaxes (pickaxe_name),
+    CONSTRAINT loadouts_fk_gliders
+        FOREIGN KEY (glider_name)
+        REFERENCES gliders (glider_name),
+    CONSTRAINT loadouts_fk_music
+        FOREIGN KEY (music_name)
+        REFERENCES music (music_name)
+);
+
 /* DATA */
 
 INSERT INTO bundles (bundle_name, bundle_id, bundle_cost, rarity, item_amount) VALUES
@@ -64,3 +116,17 @@ INSERT INTO gliders (glider_name, glider_id, rarity, cost, glider_set_name, bund
 ('Shugoki', 'Glider_Headset', 'ICON SERIES', 1200, 'SypherPK', 'SypherPK Bundle'),
 ('Champion Sail Shark', 'Glider_Shark_FNCS', 'RARE', 800, 'FNCS', 'FNCS Invitational 2022 Bundle'),
 ('Sweetner Sailshards', 'Glider_ID_304_BuffetFemale_AOF61', 'ICON SERIES', 1200, 'Ariana Grande', 'Ariana Grande Bundle');
+
+INSERT INTO emotes (emote_name, emote_id, rarity, cost, bundle_name, skin_name) VALUES
+('Masked', 'EID_SpeedDial_Mask', 'ICON SERIES', 1800, 'The Weeknd Bundle'),
+('Popular Vibe', 'EID_SpeedDial', 'ICON SERIES', 500, 'The Weeknd Bundle'),
+('I''m Diamond', 'EID_KeeperDreamChorus', 'ICON SERIES', 800, 'BTS Dynamite Pack'),
+('I''ts Dynamite!', 'EID_KeeperDreamHook', 'ICON SERIES', 800, 'BTS Dynamite Pack')
+
+INSERT INTO pickaxes (pickaxe_name, pickaxe_id, rarity, cost, pickaxe_set_name, bundle_name) VALUES
+('Zuko''s Broadswords', 'Pickaxe_TikiTorch', 'RARE', 2000, 'Avatar', 'Zuko Pack'),
+('Axe-O', 'Pickaxe_SpeedDial', 'ICON SERIES', 500, 'The Weeknd', 'The Weeknd Bundle'),
+('Quickstrike', 'Pickaxe_ID_172_BandageNinja', 'UNCOMMON', 500, 'Falcon Clan', 'Falcon Clan Bundle'),
+('Talons', 'Pickaxe_ID_219_BandageNinja1H', 'RARE', 800, 'Falcon Clan', 'Falcon Clan Bundle'),
+('Simple Sledge', 'Pickaxe_ID_213_AssassinSuitSledgehammer', 'UNCOMMON', 500, 'John Wick', 'John Wick'),
+('Wick''s Katana', 'Pickaxe_TracePaper', 'UNCOMMON', 500, 'John Wick', 'John Wick'),
