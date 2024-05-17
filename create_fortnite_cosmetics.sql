7793e6f92cc025e879542bcc69a4a50ffd3d8091
Isabelle Nguyen <66228161+odaluvr73@users.noreply.github.com>
2024-05-17 10:06:46 -0700

    Final Version
    Final Version - Restored ENUM() Datatypes

--- create_fortnite_cosmetics.sql
+++ create_fortnite_cosmetics.sql
@@ -9,14 +9,14 @@ CREATE TABLE bundles(
 	bundle_name		VARCHAR(255),
     bundle_id		VARCHAR(255)	NOT NULL		UNIQUE,
     bundle_cost		INT,
-    rarity			VARCHAR(20) 	NOT NULL, 
+    rarity			ENUM('Common', 'Uncommon', 'Rare', 'Epic', 'Legendary', 'Dark Series', 'Frozen Series', 'Lava Series', 'Shadow Series', 'Slurp Series', 'Icon Series', 'Star Wars Series', 'DC Series', 'Marvel Series', 'Gaming Legends Series', 'Lamborghini', 'McLaren')		NOT NULL, 
     item_amount		INT				NOT NULL
 );
 
 CREATE TABLE skins(
 	skin_name		VARCHAR(255),
     skin_id			VARCHAR(255)	NOT NULL		UNIQUE,
-    rarity			VARCHAR(20)		NOT NULL,
+    rarity			ENUM('Common', 'Uncommon', 'Rare', 'Epic', 'Legendary', 'Dark Series', 'Frozen Series', 'Lava Series', 'Shadow Series', 'Slurp Series', 'Icon Series', 'Star Wars Series', 'DC Series', 'Marvel Series', 'Gaming Legends Series', 'Lamborghini', 'McLaren')		NOT NULL,
     cost			INT,
     skin_set_name	VARCHAR(255)					DEFAULT NULL,
     bundle_name		VARCHAR(255)					DEFAULT NULL
@@ -25,7 +25,7 @@ CREATE TABLE skins(
 CREATE TABLE gliders(
 	glider_name		VARCHAR(255),
     glider_id		VARCHAR(255)	NOT NULL		UNIQUE,
-    rarity			VARCHAR(20)		NOT NULL,
+    rarity			ENUM('Common', 'Uncommon', 'Rare', 'Epic', 'Legendary', 'Dark Series', 'Frozen Series', 'Lava Series', 'Shadow Series', 'Slurp Series', 'Icon Series', 'Star Wars Series', 'DC Series', 'Marvel Series', 'Gaming Legends Series', 'Lamborghini', 'McLaren')		NOT NULL,
     cost			INT,
     glider_set_name	VARCHAR(255)					DEFAULT NULL,
     bundle_name		VARCHAR(255)					DEFAULT NULL
@@ -34,7 +34,7 @@ CREATE TABLE gliders(
 CREATE TABLE emotes(
 	emote_name		VARCHAR(255),
     emote_id		VARCHAR(255)	NOT NULL		UNIQUE,
-    rarity			VARCHAR(20)		NOT NULL,
+    rarity			ENUM('Common', 'Uncommon', 'Rare', 'Epic', 'Legendary', 'Dark Series', 'Frozen Series', 'Lava Series', 'Shadow Series', 'Slurp Series', 'Icon Series', 'Star Wars Series', 'DC Series', 'Marvel Series', 'Gaming Legends Series', 'Lamborghini', 'McLaren')		NOT NULL,
     cost			INT,
     bundle_name		VARCHAR(255)	DEFAULT NULL
 );
@@ -42,7 +42,7 @@ CREATE TABLE emotes(
 CREATE TABLE pickaxes(
 	pickaxe_name		VARCHAR(255),
     pickaxe_id			VARCHAR(255)	NOT NULL	    UNIQUE,
-    rarity				VARCHAR(20)		NOT NULL,
+    rarity				ENUM('Common', 'Uncommon', 'Rare', 'Epic', 'Legendary', 'Dark Series', 'Frozen Series', 'Lava Series', 'Shadow Series', 'Slurp Series', 'Icon Series', 'Star Wars Series', 'DC Series', 'Marvel Series', 'Gaming Legends Series', 'Lamborghini', 'McLaren')		NOT NULL,
     cost				INT,
     pickaxe_set_name	VARCHAR(255)	DEFAULT NULL,
     bundle_name			VARCHAR(255)	DEFAULT NULL
@@ -51,7 +51,7 @@ CREATE TABLE pickaxes(
 CREATE TABLE music(
 	music_name 			VARCHAR(255),
 	music_id 			VARCHAR(255)	NOT NULL 	UNIQUE,
-	rarity 				VARCHAR(20) 	NOT NULL,
+	rarity 				ENUM('Common', 'Uncommon', 'Rare', 'Epic', 'Legendary', 'Dark Series', 'Frozen Series', 'Lava Series', 'Shadow Series', 'Slurp Series', 'Icon Series', 'Star Wars Series', 'DC Series', 'Marvel Series', 'Gaming Legends Series', 'Lamborghini', 'McLaren') NOT NULL,
 	set_name 			VARCHAR(255) 	DEFAULT NULL,
 	song_length 		SMALLINT 		NOT NULL
 );
@@ -150,54 +150,54 @@ FOREIGN KEY (music_name) REFERENCES music (music_name);
 /* DATA */
 
 INSERT INTO bundles (bundle_name, bundle_id, bundle_cost, rarity, item_amount) VALUES
-('Zuko Pack', 'TikiTorch_Bundle', 2000, 'Rare', 3),
-('The Weeknd Bundle', 'SpeedDial_Bundle', 3500, 'Icon Series', 8),
-('Falcon Clan Bundle', 'Architect_BandageNinja_Bundle', 2800,  'Epic', 7),
-('BTS Dynamite Pack', 'KeeperDreamBundle', 800, 'Icon Series', 2),
-('John Wick', 'AssassinSuit_Bundle', 2400,  'Legendary', 6),
-('SypherPK Bundle', 'Headset_Bundle', 2800, 'Icon Series', 6),
-('FNCS Invitational 2022 Bundle', 'FNCS22_Bundle', 2300, 'Rare', 6),
-('Ariana Grande Bundle', 'Buffet_Bundle', 2800, 'Icon Series', 5);
+('Zuko Pack', 'TikiTorch_Bundle', 2000, 2, 3),
+('The Weeknd Bundle', 'SpeedDial_Bundle', 3500, 11, 8),
+('Falcon Clan Bundle', 'Architect_BandageNinja_Bundle', 2800, 4, 7),
+('BTS Dynamite Pack', 'KeeperDreamBundle', 800, 11, 2),
+('John Wick', 'AssassinSuit_Bundle', 2400, 5, 6),
+('SypherPK Bundle', 'Headset_Bundle', 2800, 11, 6),
+('FNCS Invitational 2022 Bundle', 'FNCS22_Bundle', 2300, 2, 6),
+('Ariana Grande Bundle', 'Buffet_Bundle', 2800, 11, 5);
 
 INSERT INTO skins (skin_name, skin_id, rarity, cost, skin_set_name, bundle_name) VALUES
-('Zuko', 'Character_TikiTorch',  'Epic', 2000, 'Avatar', 'Zuko Pack'),
-('The Weeknd Combat', 'Character_SpeedDialBattle', 'Icon Series', 1800, 'The Weeknd', 'The Weeknd Bundle'),
-('Kenji', 'CID_361_Athena_Commando_M_BandageNinja',  'Epic', 1500, 'Falcon Clan', 'Falcon Clan Bundle'),
-('Kuno', 'CID_362_Athena_Commando_F_BandageNinja',  'Epic', 1500, 'Falcon Clan', 'Falcon Clan Bundle'),
-('John Wick', 'CID_416_Athena_Commando_M_AssassinSuit',  'Legendary', 2000, 'John Wick', 'John Wick'),
-('Harpy Haze', 'Character_FabulousWind', 'Rare', NULL, 'ScreamingSun', NULL),
-('Lavendra', 'Character_RetroPhotographer',  'Epic', 1200, 'Flawless Flora', NULL);
+('Zuko', 'Character_TikiTorch', 4, 2000, 'Avatar', 'Zuko Pack'),
+('The Weeknd Combat', 'Character_SpeedDialBattle', 11, 1800, 'The Weeknd', 'The Weeknd Bundle'),
+('Kenji', 'CID_361_Athena_Commando_M_BandageNinja', 4, 1500, 'Falcon Clan', 'Falcon Clan Bundle'),
+('Kuno', 'CID_362_Athena_Commando_F_BandageNinja', 4, 1500, 'Falcon Clan', 'Falcon Clan Bundle'),
+('John Wick', 'CID_416_Athena_Commando_M_AssassinSuit', 5, 2000, 'John Wick', 'John Wick'),
+('Harpy Haze', 'Character_FabulousWind', 3, NULL, 'ScreamingSun', NULL),
+('Lavendra', 'Character_RetroPhotographer', 4, 1200, 'Flawless Flora', NULL);
 
 INSERT INTO gliders (glider_name, glider_id, rarity, cost, glider_set_name, bundle_name) VALUES
-('Falcon', 'Glider_ID_133_BandageNinja', 'Legendary', 1500, 'Falcon Clan', 'Falcon Clan Bundle'),
-('Appa', 'Glider_DoubleDuty',  'Epic', NULL, 'Avatar', NULL),
-('Shugoki', 'Glider_Headset', 'Icon Series', 1200, 'SypherPK', 'SypherPK Bundle'),
-('Champion Sail Shark', 'Glider_Shark_FNCS', 'Rare', 800, 'FNCS', 'FNCS Invitational 2022 Bundle'),
-('Sweetner Sailshards', 'Glider_ID_304_BuffetFemale_AOF61', 'Icon Series', 1200, 'Ariana Grande', 'Ariana Grande Bundle');
+('Falcon', 'Glider_ID_133_BandageNinja', 5, 1500, 'Falcon Clan', 'Falcon Clan Bundle'),
+('Appa', 'Glider_DoubleDuty', 4, NULL, 'Avatar', NULL),
+('Shugoki', 'Glider_Headset', 11, 1200, 'SypherPK', 'SypherPK Bundle'),
+('Champion Sail Shark', 'Glider_Shark_FNCS', 3, 800, 'FNCS', 'FNCS Invitational 2022 Bundle'),
+('Sweetner Sailshards', 'Glider_ID_304_BuffetFemale_AOF61', 11, 1200, 'Ariana Grande', 'Ariana Grande Bundle');
 
 INSERT INTO emotes (emote_name, emote_id, rarity, cost, bundle_name) VALUES
-('Masked', 'EID_SpeedDial_Mask', 'Icon Series', 1800, 'The Weeknd Bundle'),
-('Popular Vibe', 'EID_SpeedDial', 'Icon Series', 500, 'The Weeknd Bundle'),
-('I''m Diamond', 'EID_KeeperDreamChorus', 'Icon Series', 800, 'BTS Dynamite Pack'),
-('I''ts Dynamite!', 'EID_KeeperDreamHook', 'Icon Series', 800, 'BTS Dynamite Pack');
+('Masked', 'EID_SpeedDial_Mask', 11, 1800, 'The Weeknd Bundle'),
+('Popular Vibe', 'EID_SpeedDial', 11, 500, 'The Weeknd Bundle'),
+('I''m Diamond', 'EID_KeeperDreamChorus', 11, 800, 'BTS Dynamite Pack'),
+('I''ts Dynamite!', 'EID_KeeperDreamHook', 11, 800, 'BTS Dynamite Pack');
 
 INSERT INTO pickaxes (pickaxe_name, pickaxe_id, rarity, cost, pickaxe_set_name, bundle_name) VALUES
-('Zuko''s Broadswords', 'Pickaxe_TikiTorch', 'Rare', 2000, 'Avatar', 'Zuko Pack'),
-('Axe-O', 'Pickaxe_SpeedDial', 'Icon Series', 500, 'The Weeknd', 'The Weeknd Bundle'),
-('Quickstrike', 'Pickaxe_ID_172_BandageNinja', 'Rare', 500, 'Falcon Clan', 'Falcon Clan Bundle'),
-('Talons', 'Pickaxe_ID_219_BandageNinja1H', 'Rare', 800, 'Falcon Clan', 'Falcon Clan Bundle'),
-('Simple Sledge', 'Pickaxe_ID_213_AssassinSuitSledgehammer', 'Rare', 500, 'John Wick', 'John Wick'),
-('Wick''s Katana', 'Pickaxe_TracePaper', 'Rare', 500, 'John Wick', 'John Wick');
+('Zuko''s Broadswords', 'Pickaxe_TikiTorch', 3, 2000, 'Avatar', 'Zuko Pack'),
+('Axe-O', 'Pickaxe_SpeedDial', 11, 500, 'The Weeknd', 'The Weeknd Bundle'),
+('Quickstrike', 'Pickaxe_ID_172_BandageNinja', 2, 500, 'Falcon Clan', 'Falcon Clan Bundle'),
+('Talons', 'Pickaxe_ID_219_BandageNinja1H', 3, 800, 'Falcon Clan', 'Falcon Clan Bundle'),
+('Simple Sledge', 'Pickaxe_ID_213_AssassinSuitSledgehammer', 2, 500, 'John Wick', 'John Wick'),
+('Wick''s Katana', 'Pickaxe_TracePaper', 2, 500, 'John Wick', 'John Wick');
 
 INSERT INTO music (music_name, music_id, rarity, set_name, song_length) VALUES
 ('Coral Chorus', 'MusicPack_008_Coral', 3 , 'Fish Food', 149),
-('Loot In The Mountains', 'MusicPack_117_WinterFest2021', 'Rare', 'Winterfest ''21', 183),
-('Somebody This Fly', 'MusicPack_122_Sleek_3ST6M', 'Icon Series', 'Silk Sonic', 219),
-('Streets Ignite', 'MusicPack_200_JamTrack', 'Rare', 'TMNT', 173),
+('Loot In The Mountains', 'MusicPack_117_WinterFest2021', 3, 'Winterfest ''21', 183),
+('Somebody This Fly', 'MusicPack_122_Sleek_3ST6M', 11, 'Silk Sonic', 219),
+('Streets Ignite', 'MusicPack_200_JamTrack', 3, 'TMNT', 173),
 ('Caped Crusader', 'MusicPack_030_BlackMonday_X91ZH', 13, 'Gotham City', 150),
 ('Silk & Cologne (EI8HT VERSION)', 'MusicPack_179_FearlessFlight', 14, 'Across the Spider-Verse', 148),
 ('Breathing Underwater', 'MusicPack_061_RLxFN', 15, 'Rocket League', 200),
-('Flip Reset', 'MusicPack_073_RL2PromoTrack',  'Rare', 'Rocket League', 184);
+('Flip Reset', 'MusicPack_073_RL2PromoTrack', 3, 'Rocket League', 184);
 
 INSERT INTO players (player_name, player_id) VALUES
 ('ZukoGamer182', 'PID_2265_Z'),
